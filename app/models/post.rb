358655eedf96bceb43f92604604e1fc52f9fcb39
kazma-k <hecunchanghe@mbp-2.local>
2019-04-02 12:27:16 +0900

    検索フォーム完成

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,12 +2,4 @@ class Post < ApplicationRecord
   has_one_attached :image
   # created_atカラムを降順で取得する
   scope :sorted, -> { order(created_at: :desc) }
-
-  def self.search(search)
-      if search
-        Post.where(['item_name LIKE (?)', "%#{search}%"])
-      else
-        Post.all
-      end
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: "posts#index"
-  get '/posts/search', to: 'posts#search'
   get '/myindex', to: 'posts#myindex'
   devise_for :users
   resources :posts
